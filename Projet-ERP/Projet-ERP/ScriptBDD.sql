a661e8886e505e3bcc1251ee73f56b28c093d0b0
Enzo <fenzo59000@gmail.com>
2025-04-30 22:17:25 +0200

    Update ScriptBDD.sql

--- Projet-ERP/Projet-ERP/ScriptBDD.sql
+++ Projet-ERP/Projet-ERP/ScriptBDD.sql
@@ -1,37 +1,34 @@
 -- Script pour la BDD du Projet ERP
 
-CREATE TABLE Capteurs (
-    id SERIAL PRIMARY KEY,
-    id_module INTEGER NOT NULL,
+CREATE TABLE mesure_qualite_air (
+    id_mesure_qualite_air SERIAL PRIMARY KEY,
+    module_id INTEGER NOT NULL,
     temperature DOUBLE PRECISION NOT NULL,
     hygrometrie DOUBLE PRECISION NOT NULL,
     co2 DOUBLE PRECISION NOT NULL,
-    date TIMESTAMP NOT NULL
-);
-
-CREATE TABLE QualiteAir (
-    id SERIAL PRIMARY KEY,
-    id_module INTEGER NOT NULL,
     qualite_air REAL NOT NULL,
     date TIMESTAMP NOT NULL
 );
 
-CREATE TABLE MesuresVentilation (
-    id SERIAL PRIMARY KEY,
-    id_module INTEGER NOT NULL,
+CREATE TABLE zone (
+    id_zone SERIAL PRIMARY KEY,
+    module_id INTEGER NOT NULL,
+    nom text NOT NULL,
+    coordonnees_x INTEGER NOT NULL,
+    coordonnees_y INTEGER NOT NULL,
+    puissance actuelle INTEGER NOT NULL,
+    puissance demandée INTEGER NOT NULL,
+    date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE mesure_ventilation (
+    id_mesure_ventilation SERIAL PRIMARY KEY,
+    module_id INTEGER NOT NULL,
     debit_air DOUBLE PRECISION NOT NULL,
-    puissance_ventilation INTEGER NOT NULL,
     date TIMESTAMP NOT NULL
 );
 
-CREATE TABLE ParametresVentilation (
-    id SERIAL PRIMARY KEY,
-    id_module INTEGER NOT NULL,
-    puissance_demandee INTEGER NOT NULL,
-    date TIMESTAMP NOT NULL
-);
-
-CREATE TABLE Utilisateurs (
+CREATE TABLE utilisateur (
     id SERIAL PRIMARY KEY,
     login TEXT NOT NULL,
     mdp_hash TEXT NOT NULL,
