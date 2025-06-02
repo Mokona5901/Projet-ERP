6d8b849d96c375d8fc1649600fbdfe402817d70c
Enzo <fenzo59000@gmail.com>
2025-06-02 12:14:21 +0200

    Update ScriptBDD.sql

--- Projet-ERP/Projet-ERP/ScriptBDD.sql
+++ Projet-ERP/Projet-ERP/ScriptBDD.sql
@@ -2,29 +2,36 @@
 
 CREATE TABLE mesure_qualite_air (
     id_mesure_qualite_air SERIAL PRIMARY KEY,
-    module_id INTEGER NOT NULL,
+    module_id INTEGER NOT NULL, 
     temperature DOUBLE PRECISION NOT NULL,
     hygrometrie DOUBLE PRECISION NOT NULL,
     co2 DOUBLE PRECISION NOT NULL,
-    qualite_air TEXT NOT NULL,
+    qualite_air REAL NOT NULL,
     date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE capteur (
+    id_capteur SERIAL PRIMARY KEY,
+    mesure_qualite_air_id INTEGER NOT NULL,
+    sonoff_ip TEXT NOT NULL
 );
 
 CREATE TABLE zone (
     id_zone SERIAL PRIMARY KEY,
     module_id INTEGER NOT NULL,
-    nom text NOT NULL,
+    nom TEXT NOT NULL,
     coordonnees_x INTEGER NOT NULL,
     coordonnees_y INTEGER NOT NULL,
-    puissance_actuelle INTEGER NOT NULL,
-    puissance_demandee INTEGER NOT NULL,
+    adresse_ip_ventilateur TEXT NOT NULL,
+    puissance_actuelle_pourcentage INTEGER NOT NULL,
+    puissance_demandee_pourcentage INTEGER NOT NULL,
     date TIMESTAMP NOT NULL
 );
 
 CREATE TABLE mesure_ventilation (
     id_mesure_ventilation SERIAL PRIMARY KEY,
     module_id INTEGER NOT NULL,
-    debit_air DOUBLE PRECISION NOT NULL,
+    debit_air REAL NOT NULL,
     date TIMESTAMP NOT NULL
 );
 
